79fb661f475c508f866098babd03b2e9ce47f660
xCoder <xcoder@xCoders-iMac.local>
2023-03-27 23:55:54 +0600

    first commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h1>Working....</h1>
+
+            </div>
+            <div className="summery-container">
+                <h2>Summary coming soon..</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
