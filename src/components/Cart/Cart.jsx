be799ae5cdbb1e6fca1d918ab8beeca22373b214
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-03-28 19:15:02 +0600

    cart fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import './Cart.css';
 const Cart = ({ cart }) => {
 
     // const { cart } = props; //Destructuring method using 
+    console.log(cart);
 
     let total = 0;
     for (const product of cart) {
@@ -14,7 +15,7 @@ const Cart = ({ cart }) => {
         <div>
             <div className="cart">
                 <h5>Order Summary</h5>
-                <p>Selected Items: {cart}</p>
+                <p>Selected Items: {cart.length}</p>
                 <p>Total Price:{total}</p>
                 <p>Total Shipping Charge:</p>
                 <p>Tax:</p>
