99b10b593d316e248daeeae295c16a894f66819f
xCoder <xcoder@xCoders-iMac.local>
2023-05-07 23:16:49 +0600

    npm update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom"
+import App from './App'
 
 const router = createBrowserRouter([
   {
-
+    path: '/',
+    element: <App />
   }
 ])
 
